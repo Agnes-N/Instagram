436e2b6466ec0b49ccb20e73816e417aace9d2cf
agnes <aggyreina@gmail.com>
2019-10-16 10:54:28 +0200

    creates profile model

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    # user = 
+    firstName = models.CharField(max_length = 30)
+    lastName = models.CharField(max_length = 30)
+    bio = models.CharField(max_length = 300)
+    profile_pic = models.ImageField(upload_to = 'pictures/', null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
