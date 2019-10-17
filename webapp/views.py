6df6ca94da978dcef76983af66d0ef2577f9f331
agnes <aggyreina@gmail.com>
2019-10-17 15:34:40 +0200

    creates function to upload an image

--- webapp/views.py
+++ webapp/views.py
@@ -11,3 +11,18 @@ def welcome(request):
     insta_users = Profile.get_all_instagram_users()
     return render(request, 'welcome.html', {"all_images":all_images, "insta_users":insta_users})
 
+
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            image_caption = form.save(commit=False)
+            image_caption.user = current_user
+            image_caption.save()
+        return redirect('welcome')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'upload.html', {"form": form})
